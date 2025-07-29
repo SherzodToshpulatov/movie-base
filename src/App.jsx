659853645a035eb2af1af7b4381d6fb1b2287b29
Sherzod Toshpulatov <sherzodtoshpolatov5@gmail.com>
2025-07-29 17:14:19 +0500

    course finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,91 @@
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useDebounce } from 'react-use';
 import './App.css'
 import Search from './components/Search'
+import Spinner from './components/Spinner';
+import MovieCards from './components/MovieCards';
+import { getTrendingMovies, updatesearchCount } from './appwrite';
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
 
 function App() {
   const[searchTerm, setSearchTerm]=useState('');
+  const[errorMessage, setErrormessage] =useState('');
+  const[movieList, setMovieList] = useState([]);
+  const[trendingMovies, setTrendingMovies] = useState([]);
+  const[isLoading, setIsLoading] = useState(false);
+  const[debouncedSearchTerm, setDeboouncedSearchterm] = useState('')
 
+  useDebounce(()=> setDeboouncedSearchterm(searchTerm), 500, [searchTerm])
+
+  const fetchMovies = async (query = '') => {
+    setIsLoading(true);
+    setErrormessage('');
+    try{
+      const endpoint = query ? 
+      `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+      : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+      const response = await fetch(endpoint, API_OPTIONS);  
+
+      if(!response.ok){
+        throw new Error('Failed to fetch movies')
+      }
+
+      const data = await response.json();
+      console.log(data)
+
+      if(data.Response === 'False'){
+        setErrormessage(data.Error || 'Failed to fetch movies');
+        setMovieList([]);
+        return;
+      }
+
+      setMovieList(data.results || [])
+
+      if(query && data.results.length > 0){
+       await updatesearchCount(query, data.results[0]);
+
+      }
+
+    }catch(error){
+      console.error(`Error fetching movies: ${error}`)
+      setErrormessage('Error fetching movies. Please try again later.')
+    }finally{
+      setIsLoading(false)
+    }
+  }
+
+  const loadTrendingMovies = async () => {
+    try{
+      const movies = await getTrendingMovies();
+
+      setTrendingMovies(movies);
+    }catch(error){
+      console.log(`Error fetching trending movies: ${error}`);
+      
+    }
+  }
+
+useEffect(()=>{
+  fetchMovies(debouncedSearchTerm);
+}, [debouncedSearchTerm])
+
+
+useEffect(()=>{
+  loadTrendingMovies();
+}, [])
   return (
     <>
       <main>
@@ -14,10 +94,42 @@ function App() {
         <div className='wrapper'>
           <header>
             <img src="./hero.png" alt="Hero Banner"/>
+
+            <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+
+            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
           </header>
-          <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+          {trendingMovies.length > 0 && (
+            <section className='trending'>
+              <h2>Trending Movies</h2>
+              <ul>
+                {trendingMovies.map((movie, index)=>(
+                  <li key={movie.$id}>
+                      <p>{index + 1}</p>
+                      <img src={movie.poster_url} alt={movie.title} />
+                  </li>
+                ))}
+              </ul>
+            </section>
+          )}
+          <section className='all-movies'>
+            <h2>All Movies</h2>
+
+            {isLoading?(
+                <Spinner/>
+              ):errorMessage ? (
+                <p className='text-red-500'>{errorMessage}</p>
+              ) : (
+                <ul>
+                  {movieList.map((movie) => (
+                    <MovieCards key={movie.id} movie={movie}/>
+                  ))}
+                </ul>
+              )
+            }
+          </section>
         </div>
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        
      </main>
     </>
   )
